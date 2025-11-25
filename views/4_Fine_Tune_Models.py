e8030d37f3f7f26f090c8fc3728f9bebacaf2bf1
sambra95 <sbradley.95@outlook.com>
2025-11-25 10:41:54 +0100

    disable fine tune panel if there are no masks

--- views/4_Fine_Tune_Models.py
+++ views/4_Fine_Tune_Models.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from panels import fine_tune_panel
+from helpers.state_ops import ordered_keys
+import numpy as np
 
 # Warning if no images have been uploaded yet
 if st.session_state["images"] == {}:
     st.warning("⚠️ Please upload an image on the 'Upload Models and Data' tab first.")
+    st.stop()
+
+# warning if no images have masks
+if not any(np.any(st.session_state["images"][k]["masks"]) for k in ordered_keys()):
+    st.warning("⚠️ Please upload or create masks for at least one image.")
     st.stop()
 
 col1, col2 = st.columns([1, 1])
